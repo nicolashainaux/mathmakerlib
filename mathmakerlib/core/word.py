568bae3b2a95276b90e34b53cf5b363c1bf31ca0
Nicolas Hainaux <nh.techn@gmail.com>
2017-10-31 16:40:01 +0100

    Add a simple Word class (a str that's also a concrete Printable).

---
+++ mathmakerlib/core/word.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+# Mathmaker Lib offers lualatex-printable mathematical objects.
+# Copyright 2006-2017 Nicolas Hainaux <nh.techn@gmail.com>
+
+# This file is part of Mathmaker Lib.
+
+# Mathmaker Lib is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+
+# Mathmaker Lib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Mathmaker Lib; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+from mathmakerlib.core.printable import Printable
+
+
+class Word(str, Printable):
+
+    def imprint(self, start_expr=True, variant='latex'):
+        return str(self)
