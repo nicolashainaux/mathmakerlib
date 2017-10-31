29d7fc41d68e99ff5cceea1700056f513f84e3ab
Nicolas Hainaux <nh.techn@gmail.com>
2017-10-31 16:41:38 +0100

    Add a Unit class.

---
+++ mathmakerlib/calculus/unit.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+# Mathmaker Lib offers lualatex-printable mathematical objects.
+# Copyright 2006-2017 Nicolas Hainaux <nh.techn@gmail.com>
+
+# This file is part of Mathmaker Lib.
+
+# Mathmaker Lib is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+
+# Mathmaker Lib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Mathmaker Lib; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+from mathmakerlib.core.word import Word
+from mathmakerlib.calculus.exponented import Exponented
+
+LENGTH_UNITS = ['km', 'hm', 'dam', 'm', 'dm', 'cm', 'mm', 'µm', 'nm', 'pm']
+CAPACITY_UNITS = ['kL', 'hL', 'daL', 'L', 'dL', 'cL', 'mL', 'µL', 'nL', 'pL']
+MASS_UNITS = ['kg', 'hg', 'dag', 'g', 'dg', 'cg', 'mg', 'µg', 'ng', 'pg']
+PHYSICAL_QUANTITIES = {'length': LENGTH_UNITS,
+                       'capacity': CAPACITY_UNITS,
+                       'mass': MASS_UNITS}
+COMMON_LENGTH_UNITS = LENGTH_UNITS[:-3]
+COMMON_CAPACITY_UNITS = CAPACITY_UNITS[1:-3]
+COMMON_MASS_UNITS = MASS_UNITS[:-3]
+ANGLE_UNITS = ['\\textdegree']
+CURRENCY_UNITS = ['€', '\officialeuro',
+                  '$', '\\textdollar',
+                  '£', '\\textsterling']
+CURRENCIES_DICT = {'euro': '\officialeuro',
+                   'dollar': '\\textdollar',
+                   'sterling': '\\textsterling'}
+AVAILABLE_UNITS = LENGTH_UNITS + CAPACITY_UNITS + MASS_UNITS + ANGLE_UNITS\
+    + CURRENCY_UNITS
+UNIT_KINDS = {'length': COMMON_LENGTH_UNITS,
+              'mass': COMMON_MASS_UNITS,
+              'capacity': COMMON_CAPACITY_UNITS,
+              'currency': CURRENCY_UNITS}
+
+
+class Unit(Exponented):
+
+    def __init__(self, content, exponent=None):
+        Exponented.__init__(self, Word(content), exponent=exponent)
