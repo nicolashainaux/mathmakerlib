e81c5c7f5994a88d31a5b1f3995e6fc453abf8db
Nicolas Hainaux <nh.techn@gmail.com>
2017-11-12 07:41:02 +0100

    Gather two tests in tests/drawable_test.py

---
+++ tests/core/drawable_test.py
@@ -0,0 +1,96 @@
+# -*- coding: utf-8 -*-
+
+# Mathmaker Lib offers lualatex-printable mathematical objects.
+# Copyright 2006-2017 Nicolas Hainaux <nh.techn@gmail.com>
+
+# This file is part of Mathmaker Lib.
+
+# Mathmaker Lib is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+
+# Mathmaker Lib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Mathmaker Lib; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import pytest
+
+from mathmakerlib import pkg_required
+from mathmakerlib.geometry.point import Point
+from mathmakerlib.geometry.linesegment import LineSegment
+
+
+@pytest.fixture()
+def A():
+    return Point(0, 0, 'A')
+
+
+@pytest.fixture()
+def E():
+    return Point(1, 0, 'E')
+
+
+def test_scale(A, E):
+    """Check drawing is correct."""
+    ls = LineSegment(A, E)
+    assert ls.scale == 1
+    ls.scale = 2
+    assert ls.scale == 2
+    assert ls.drawn == r"""
+\begin{tikzpicture}[scale=2]
+% Declare Points
+\coordinate (A) at (0,0);
+\coordinate (E) at (1,0);
+
+% Draw Points
+\draw (A) node {$\times$};
+\draw (E) node {$\times$};
+
+% Draw Line Segment
+\draw[thick] (A) -- (E);
+
+% Label Points
+\draw (A) node[left] {A};
+\draw (E) node[right] {E};
+\end{tikzpicture}
+"""
+    with pytest.raises(TypeError) as excinfo:
+        ls.scale = '2'
+    assert str(excinfo.value) == 'The scale must be a number.'
+
+
+def test_colors():
+    """Check colors setting and usage."""
+    p = Point(0, 0, 'A')
+    pkg_required.xcolor_options = []
+    pkg_required.xcolor = False
+    p.color = 'blue'
+    assert not pkg_required.xcolor
+    assert 'dvipsnames' not in pkg_required.xcolor_options
+    with pytest.raises(ValueError) as excinfo:
+        p.color = 'UndefinedBlueOrange'
+    assert str(excinfo.value) == 'Unknown color name: UndefinedBlueOrange. ' \
+        'Only colors from xcolor\'s dvipsnames are yet supported.'
+    assert not pkg_required.xcolor
+    assert 'dvipsnames' not in pkg_required.xcolor_options
+    p.color = 'Apricot'
+    assert pkg_required.xcolor
+    assert 'dvipsnames' in pkg_required.xcolor_options
+    assert p.drawn == r"""
+\begin{tikzpicture}
+% Declare Point
+\coordinate (A) at (0,0);
+
+% Draw Point
+\draw[Apricot] (A) node {$\times$};
+
+% Label Point
+\draw (A) node[below] {A};
+\end{tikzpicture}
+"""
