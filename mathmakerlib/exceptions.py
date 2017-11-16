1664deda63114ae9e51697f536aaf24e07a68a49
Nicolas Hainaux <nh.techn@gmail.com>
2017-11-16 14:28:46 +0100

    Add MathmakerLib's exceptions.

---
+++ mathmakerlib/exceptions.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+# Mathmaker Lib offers lualatex-printable mathematical objects.
+# Copyright 2006-2017 Nicolas Hainaux <nh.techn@gmail.com>
+
+# This file is part of Mathmaker Lib.
+
+# Mathmaker Lib is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+
+# Mathmaker Lib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Mathmaker Lib; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+
+class MathmakerLibError(Exception):
+    """Basic exception for errors raised by Mathmaker Lib."""
+    def __init__(self, msg=None):
+        if msg is None:
+            # Set some default useful error message
+            msg = 'An error occured in Mathmaker Lib'
+        super().__init__(msg)
+
+
+class StopCalculation(MathmakerLibError):
+    """When no further calculation can be made."""
+    def __init__(self, o, msg=None):
+        if msg is None:
+            msg = 'No further calculation can be done on {}.'.format(repr(o))
+        super().__init__(msg=msg)
+
+
+class StopFractionReduction(StopCalculation):
+    """When no further reduction can be made."""
+    def __init__(self, f, msg=None):
+        super().__init__(f,
+                         msg='{} can no further be reduced.'.format(repr(f)))
---
+++ tests/main/exceptions_test.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+# Mathmaker Lib offers lualatex-printable mathematical objects.
+# Copyright 2006-2017 Nicolas Hainaux <nh.techn@gmail.com>
+
+# This file is part of Mathmaker Lib.
+
+# Mathmaker Lib is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+
+# Mathmaker Lib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Mathmaker Lib; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import pytest
+
+from mathmakerlib.calculus import Number
+from mathmakerlib.exceptions import MathmakerLibError, StopCalculation
+
+
+def test_MainError():
+    """Check the main mathmakerlib exception."""
+    with pytest.raises(MathmakerLibError) as excinfo:
+        raise MathmakerLibError
+    assert str(excinfo.value) == 'An error occured in Mathmaker Lib'
+
+
+def test_StopCalculation():
+    """Check StopCalculation exception."""
+    with pytest.raises(StopCalculation) as excinfo:
+        raise StopCalculation(Number('7.6'))
+    assert str(excinfo.value) == 'No further calculation can be done on ' \
+        'Number(\'7.6\').'
