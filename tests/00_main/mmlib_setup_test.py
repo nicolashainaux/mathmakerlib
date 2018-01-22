62b7697a512adaa2cf70c7755a5882cd5e59d234
Nicolas Hainaux <nh.techn@gmail.com>
2018-01-22 17:03:42 +0100

    Add a test.

---
+++ tests/00_main/mmlib_setup_test.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# Mathmaker Lib offers lualatex-printable mathematical objects.
+# Copyright 2006-2017 Nicolas Hainaux <nh.techn@gmail.com>
+
+# This file is part of Mathmaker Lib.
+
+# Mathmaker Lib is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# any later version.
+
+# Mathmaker Lib is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with Mathmaker Lib; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+import pytest
+
+from mathmakerlib import mmlib_setup
+
+
+def test_setup_errors():
+    """Check exceptions raised by mmlib_setup."""
+    with pytest.raises(TypeError) as excinfo:
+        mmlib_setup.angles.DEFAULT_ARMSPOINTS_POSITION = 'a'
+    assert str(excinfo.value) == 'DEFAULT_ARMSPOINTS_POSITION must be a '\
+        'number, found <class \'str\'> instead.'
