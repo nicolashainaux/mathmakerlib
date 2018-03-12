b87f3c694b3056dea11aa317e53c096458429ded
Nicolas Hainaux <nh.techn@gmail.com>
2018-03-12 18:09:08 +0100

    Add 'placeins' to possible detectable LaTeX packages.

--- mathmakerlib/required.py
+++ mathmakerlib/required.py
@@ -29,7 +29,7 @@ def init():
     package = {pkg_name: False
                for pkg_name in ['tikz', 'siunitx', 'xcolor', 'eurosym',
                                 'amsmath', 'stackengine', 'scalerel',
-                                'cancel', 'multicol']}
+                                'cancel', 'multicol', 'placeins']}
     options = {'xcolor': set()}
     tikz_library = {'angles': False,
                     'decorations.markings': False,
